7f90c4e72c4447ea0de2758cda0a7d006390ab51
ALichoshva <sashalihoshva@gmail.com>
2019-03-03 09:21:56 +0200

    add social-app

---
+++ HW17-20/src/js/components/news.component.js
@@ -0,0 +1,47 @@
+import { NewsService } from '../services/news.service';
+import { AuthService } from '../services/auth.service';
+import { Routing } from '../core/routing.service';
+
+export class NewsComponent {
+    constructor() {
+        this._newsService = new NewsService();
+        this._authService = new AuthService();
+        this._routing = new Routing();
+        this._news
+    }
+
+    async beforeRender() {
+        if (!this._authService.token) {
+            this._routing.navigate(`/login`);
+            throw 'Please login to acces private data';
+        }
+
+        this._news = await this._newsService.getNews(this._authService.token);
+    }
+
+    render() {
+        return `
+        <style>
+            ${this._style()}
+        </style>
+        <div><img src="${this._news['news'][0]['pictures'][0]['url']}"></div>
+        `
+    }
+
+    _style() {
+        return `
+            img {
+                max-width: 100%;
+                height: 300px;
+            }
+
+            div {
+                text-align: center;
+            }
+        `;
+    }
+
+    afterRender() {
+
+    }
+}
